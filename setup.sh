a80afddb4a700ba2f3c4348d3a1bbc53ecb7e7c5
Rafael Miranda <rafael.miranda@godeploy.com>
2023-03-07 17:12:50 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Create workspace
 
 echo "Create an Azure Machine Learning workspace:"
-az ml workspace create --name "mlw-dp100-labs" -g "$RGName"
+az ml workspace create --name "mlw-dp100-labs" -g "DP-100"
 
 # Create compute instance
 guid=$(cat /proc/sys/kernel/random/uuid)
@@ -13,4 +13,4 @@ suffix=${suffix:0:18}
 ComputeName="ci${suffix}"
 
 echo "Creating a compute instance with name: " $ComputeName
-az ml compute create --name ${ComputeName} --size STANDARD_DS3_V2 --type ComputeInstance -w mlw-dp100-labs -g "$RGName"
+az ml compute create --name ${ComputeName} --size STANDARD_DS3_V2 --type ComputeInstance -w mlw-dp100-labs -g "DP-100"
